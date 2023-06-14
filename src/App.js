5c8eea745fb72aa3c459412543845af19c8e07ad
mehmet çoban <74604824+mcban34@users.noreply.github.com>
2023-06-14 16:18:15 +0300

    router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import Products from './Components/Product/Products';
+import { Routes,Route,Link } from 'react-router-dom';
+import Home from './Components/Pages/Home';
+import Contact from './Components/Pages/Contact';
+import Team from './Components/Pages/Team';
 
 
 function App() {
   return (
     <div className="App">
-      <Products />
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/team">Team</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/team' element={<Team/>} />
+      </Routes>
     </div>
   );
 }
