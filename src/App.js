a47af0b0bf2d742303049c5c987b03bf3809de0d
mehmet çoban <74604824+mcban34@users.noreply.github.com>
2023-06-17 23:20:00 +0300

    create build

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React from 'react';
 import { Routes,Route,Link } from 'react-router-dom';
 import Detail from './Components/ProductDetail/Detail';
 import Home from './Components/ProductDetail/Home';
+import Team from './Components/Pages/Team'
 
 function App() {
   return (
       <div className='App'>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/team'>Team</Link>
+            </li>
+          </ul>
+        </nav>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/detail/:id' element={<Detail />} />
+          <Route path='/team' element={<Team />} />
         </Routes>
       </div>
   );
